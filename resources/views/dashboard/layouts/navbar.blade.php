8f746bc1fc58a318f127dd0e147d8ae6edf816f2
abidalazis <115638163+abidalazis@users.noreply.github.com>
2024-01-15 10:48:27 +0700

    edit navbar dan sidebar

--- resources/views/dashboard/layouts/navbar.blade.php
+++ resources/views/dashboard/layouts/navbar.blade.php
@@ -2,12 +2,12 @@
     <div class="flex-shrink-0 p-3" style="width: 280px;">
         <a href="/laporan-service" class="d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom">
           <svg class="bi pe-none me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
-          <span class="fs-5 fw-semibold">Laporan</span>
+          <span class="fs-5 fw-semibold">Service AC</span>
         </a>
         <ul class="list-unstyled ps-0">
           <li class="mb-1">
             <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-              Data Master
+              Data Master Service AC
             </button>
             <div class="collapse show" id="home-collapse">
               <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
