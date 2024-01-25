416f6c049f1fb6d3dd7ac42e49ee84b2352f106b
abidalazis <115638163+abidalazis@users.noreply.github.com>
2024-01-25 11:04:46 +0700

    tambah chart unit

--- app/Http/Controllers/indikatorController.php
+++ app/Http/Controllers/indikatorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\laporan_it;
 
 class indikatorController extends Controller
 {
@@ -25,7 +26,24 @@ class indikatorController extends Controller
     }
     public function dashboard_laporan()
     {
-        return view('laporan.index');
+        // $data_laporan = laporan_it::get();
+        // $data_unit =[];
+        // foreach ($data_laporan as $dl) {
+        //     $data[] = $dl->unit;
+        // }
+        // $unit = laporan_it::select(laporan_it::raw("select COUNT(*) as total unit, unit from laporan_its group by unit"));
+        $unitCounts = Laporan_it::selectRaw('COUNT(*) as total_unit, unit')
+                    ->groupBy('unit')
+                    ->get();
+        $dataChart="";
+        foreach ($unitCounts as $list) {
+            $dataChart.="['".$list->unit."',".$list->total_unit."],";
+        }
+       $arr['dataChart']=rtrim($dataChart,",");
+        // dd($dataChart);
+        
+        // dd($data);
+        return view('laporan.index',$arr);
     }
 
     
--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -1,4 +1,27 @@
 @extends('laporan.layouts.main')
+@push('chart')
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      <?php echo $dataChart?>
+    ]);
+
+    var options = {
+      title: 'My Daily Activities'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+  }
+</script>
+@endpush
 @section('content')
 <div class="container-fluid px-4">
     <h1 class="mt-4">Laporan Service</h1>
@@ -10,74 +33,11 @@
         <div class="card-header">
             <i class="fas fa-table me-1"></i>
             DataTable Example
-        </div>
+        </div> 
+        <div id="piechart" style="width: 900px; height: 500px;"></div>
     </div>
-    <div class="card mb-4">
-        <div class="card-header">
-            <i class="fas fa-chart-area me-1"></i>
-            Area Chart Example
-        </div>
-        <div class="card-body"><canvas id="myAreaChart" width="100%" height="30"></canvas></div>
-        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-    </div>
+   
+  </div>
 </div>    
 @endsection
-@push('chart')
-    
 
-<script>// Set new default font family and font color to mimic Bootstrap's default styling
-    Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-    Chart.defaults.global.defaultFontColor = '#292b2c';
-    
-    // Area Chart Example
-    var ctx = document.getElementById("myAreaChart");
-    var myLineChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
-        datasets: [{
-          label: "Sessions",
-          lineTension: 0.3,
-          backgroundColor: "rgba(2,117,216,0.2)",
-          borderColor: "rgba(2,117,216,1)",
-          pointRadius: 5,
-          pointBackgroundColor: "rgba(2,117,216,1)",
-          pointBorderColor: "rgba(255,255,255,0.8)",
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(2,117,216,1)",
-          pointHitRadius: 50,
-          pointBorderWidth: 2,
-          data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-        }],
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            time: {
-              unit: 'date'
-            },
-            gridLines: {
-              display: false
-            },
-            ticks: {
-              maxTicksLimit: 7
-            }
-          }],
-          yAxes: [{
-            ticks: {
-              min: 0,
-              max: 40000,
-              maxTicksLimit: 5
-            },
-            gridLines: {
-              color: "rgba(0, 0, 0, .125)",
-            }
-          }],
-        },
-        legend: {
-          display: false
-        }
-      }
-    });
-    </script>
-    @endpush
